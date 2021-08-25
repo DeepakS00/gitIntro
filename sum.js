48d252fdc03e56fe0f6a5e2655b30e58ebd55165
Deepak Shukla <deepak.shukla@newput.com>
2021-08-25 16:49:29 +0530

    added message to sum

--- sum.js
+++ sum.js
@@ -2,6 +2,7 @@ const first = 54;
 const second = 454;
 
 function sum(a,b){
-    return a+b;
+    const result = 'The sum of given numbers is: ' + (a+b)+'.';
+    console.log(result);
 }
 sum(first,second);
